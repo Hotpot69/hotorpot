a938df8380d3ab7112a15d9474b8bd3b2600cfae
Hotpot69 <155320337+Hotpot69@users.noreply.github.com>
2023-12-31 23:09:48 +0530

    Add files via upload

---
+++ link.js
@@ -0,0 +1,161 @@
+// Define an array of user objects with their information
+
+
+// If any one is reading this -- msg for you------>>   ((ur a bitch!!!   XD 😂🤣))
+	
+	
+
+const removeButton = document.querySelector('.btn');
+const card = document.querySelector('.card');
+
+removeButton.addEventListener('click', function() {
+  card.style.display = 'none';
+});
+
+const removeButtonN = document.querySelector('.btnN');
+const cardN = document.querySelector('.card');
+
+removeButtonN.addEventListener('click', function() {
+  cardN.style.display = 'none';
+});
+
+
+
+
+
+
+
+const removeButton3 = document.querySelector('.btn3');
+const card3 = document.querySelector('.card3');
+
+removeButton3.addEventListener('click', function() {
+  card3.style.display = 'none';
+});
+const removeButton3N = document.querySelector('.btn3N');
+const card3N = document.querySelector('.card3');
+
+removeButton3N.addEventListener('click', function() {
+  card3N.style.display = 'none';
+});
+
+
+
+
+
+const removeButton4 = document.querySelector('.btn4');
+const card4 = document.querySelector('.card4');
+
+removeButton4.addEventListener('click', function() {
+  card4.style.display = 'none';
+});
+const removeButton4N = document.querySelector('.btn4N');
+const card4N = document.querySelector('.card4');
+
+removeButton4N.addEventListener('click', function() {
+  card4N.style.display = 'none';
+});
+
+
+
+
+
+
+const removeButton5 = document.querySelector('.btn5');
+const card5 = document.querySelector('.card5');
+
+removeButton5.addEventListener('click', function() {
+  card5.style.display = 'none';
+});
+const removeButton5N = document.querySelector('.btn5N');
+const card5N = document.querySelector('.card5');
+
+removeButton5N.addEventListener('click', function() {
+  card5N.style.display = 'none';
+});
+
+
+
+
+const removeButton6 = document.querySelector('.btn6');
+const card6 = document.querySelector('.card6');
+
+removeButton6.addEventListener('click', function() {
+  card6.style.display = 'none';
+});
+const removeButton6N = document.querySelector('.btn6N');
+const card6N = document.querySelector('.card6');
+
+removeButton6N.addEventListener('click', function() {
+  card6N.style.display = 'none';
+});
+
+
+const removeButton9 = document.querySelector('.btn9');
+const card9 = document.querySelector('.card9');
+
+removeButton9.addEventListener('click', function() {
+  card9.style.display = 'none';
+});
+const removeButton9N = document.querySelector('.btn9N');
+const card9N = document.querySelector('.card9');
+
+removeButton9N.addEventListener('click', function() {
+  card9N.style.display = 'none';
+});
+
+
+
+const removeButton10 = document.querySelector('.btn10');
+const card10 = document.querySelector('.card10');
+
+removeButton10.addEventListener('click', function() {
+  card10.style.display = 'none';
+});
+const removeButton10N = document.querySelector('.btn10N');
+const card10N = document.querySelector('.card10');
+
+removeButton10N.addEventListener('click', function() {
+  card10N.style.display = 'none';
+});
+
+
+const removeButton11 = document.querySelector('.btn11');
+const card11 = document.querySelector('.card11');
+
+removeButton11.addEventListener('click', function() {
+  card11.style.display = 'none';
+});
+const removeButton11N = document.querySelector('.btn11N');
+const card11N = document.querySelector('.card11');
+
+removeButton11N.addEventListener('click', function() {
+  card11N.style.display = 'none';
+});
+
+
+
+const removeButton12 = document.querySelector('.btn12');
+const card12 = document.querySelector('.card12');
+
+removeButton12.addEventListener('click', function() {
+  card12.style.display = 'none';
+});
+const removeButton12N = document.querySelector('.btn12N');
+const card12N = document.querySelector('.card12');
+
+removeButton12N.addEventListener('click', function() {
+  card12N.style.display = 'none';
+});
+
+
+
+
+function openWebsite(url) {
+  window.open(url, '_blank');
+}
+
+function openWebsite1(url) {
+  window.open(url, '_blank');
+}
+
+
